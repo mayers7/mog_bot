7245097a74725d5f7d99cb34d7f5e8830fdf2083
mayers7 <mayers7@gmu.edu>
2025-03-16 21:59:52 -0400

    Update mog_bot.py

--- mog_bot.py
+++ mog_bot.py
@@ -12,11 +12,11 @@ time.sleep(2) #keeps us from hitting reddit API limits
 
 # Reddit API Credentials
 reddit = praw.Reddit(
-    client_id=os.getenv("REDDIT_CLIENT_ID"),
-    client_secret=os.getenv("REDDIT_CLIENT_SECRET"),
-    user_agent=os.getenv("REDDIT_USER_AGENT"),
-    username=os.getenv("REDDIT_USERNAME"),
-    password=os.getenv("REDDIT_PASSWORD"),
+    client_id=os.getenv(REDDIT_CLIENT_ID),
+    client_secret=os.getenv(REDDIT_CLIENT_SECRET),
+    user_agent=os.getenv(REDDIT_USER_AGENT),
+    username=os.getenv(REDDIT_USERNAME),
+    password=os.getenv(REDDIT_PASSWORD),
 )
 
 # Define the subreddit
